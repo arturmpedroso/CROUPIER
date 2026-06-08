09a42133e2c3b1afe2a5d4d7b8db18f13533cc36
arturmpedroso <arturpedroso@alunos.utfpr.edu.br>
2026-06-08 17:06:05 -0300

    validacao no cadastro de usuario

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Bloqueia e limpa campos estranhos enviados pelo front que não estão no DTO
+    forbidNonWhitelisted: true, // Dispara erro se tentarem enviar campos não permitidos
+  }));
 
   app.enableCors({
     origin: 'http://localhost:3000',
