b24ceb30b1ccda0869c8e23e82daf8a04c7dc11b
arturmpedroso <arturpedroso@alunos.utfpr.edu.br>
2026-07-04 14:43:29 -0300

    ajustando swagger para o login

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,32 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+
+@ApiTags('Auth (Autenticação)') // Cria a aba de Autenticação no painel
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  @HttpCode(HttpStatus.OK) 
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Realiza a autenticação do usuário (Login)' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Login efetuado com sucesso. Retorna o token JWT e os dados do usuário.' 
+  })
+  @ApiResponse({ status: 401, description: 'Credenciais inválidas (E-mail ou senha incorretos).' })
+  // Avisa ao Swagger quais campos ele deve desenhar na caixinha de testes
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', example: 'artur@croupier.com', description: 'E-mail do usuário cadastrado' },
+        password: { type: 'string', example: 'SenhaMesa123', description: 'Senha de acesso' }
+      },
+      required: ['email', 'password']
+    }
+  })
   async login(@Body() loginDto: any) {
     return this.authService.login(loginDto);
   }
