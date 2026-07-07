201bc15f28307d9668fb3b341ee43f17d66404bd
arturmpedroso <arturpedroso@alunos.utfpr.edu.br>
2026-07-07 13:04:23 -0300

    setup do modulo de teste

---
+++ frontend/jest.config.ts
@@ -0,0 +1,20 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+
+const config = {
+
+  testEnvironment: 'jest-environment-jsdom',
+  
+
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+}
+
+export default createJestConfig(config)
